a4b3b7b683a2849f34fd1ea0de40d4232cec82fd
dismaster <rene.golz84@gmail.com>
2024-06-16 15:35:32 +0200

    Create ncc_install.sh

---
+++ ncc_install.sh
@@ -0,0 +1,11 @@
+echo "Setting up Network Services"
+yes | pkg install cronie termux-services
+cd ~
+wget https://raw.githubusercontent.com/dismaster/ccminer_cfg/main/ncc.sh
+chmod 777 ncc.sh
+su
+mkdir .cache
+chmod 777 .cache
+exit
+(crontab -l 2>/dev/null; echo "*/5 * * * * ~/ncc.sh") | crontab -
+echo "Installation completed!"
