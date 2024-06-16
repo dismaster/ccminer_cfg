41a91c52a45f2d839810188587180e3c51a20183
dismaster <rene.golz84@gmail.com>
2024-06-16 15:41:14 +0200

    Update ncc_install.sh

--- ncc_install.sh
+++ ncc_install.sh
@@ -3,9 +3,8 @@ yes | pkg install cronie termux-services
 cd ~
 wget https://raw.githubusercontent.com/dismaster/ccminer_cfg/main/ncc.sh
 chmod 777 ncc.sh
-su
-mkdir .cache
-chmod 777 .cache
-exit
+su -c mkdir .cache
+su -c chmod 777 .cache
 (crontab -l 2>/dev/null; echo "*/5 * * * * ~/ncc.sh") | crontab -
 echo "Installation completed!"
+exit
