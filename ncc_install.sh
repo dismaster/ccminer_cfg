11e56e1a3ecf77cfb83a669231c62f0c9abf486c
dismaster <rene.golz84@gmail.com>
2024-06-18 08:33:21 +0200

    Update ncc_install.sh

--- ncc_install.sh
+++ ncc_install.sh
@@ -5,9 +5,8 @@ cd ~
 rm ncc.sh
 wget https://raw.githubusercontent.com/dismaster/ccminer_cfg/main/ncc.sh
 chmod 777 ncc.sh
-su -c mkdir .cache
-su -c chown u0_a179:u0_a179 .cache
-su -c chmod 777 .cache
+mkdir .cache
+chmod 777 .cache
 (crontab -l 2>/dev/null; echo "*/5 * * * * ~/ncc.sh") | crontab -
 echo "Installation completed!"
 exit
